82f376afa180586d1abd00176e3c0e54554056dc
tam11a <ibrahimsadiktamim@gmail.com>
2022-12-25 03:48:05 +0600

    added: button

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,12 @@
 const Home = () => {
 	return (
 		<div>
-			<h1 className="text-3xl font-bold underline ">Hello world!</h1>
+			<h1 className="text-3xl text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+				Hello world!
+			</h1>
+			<button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
+				Save Changes
+			</button>
 		</div>
 	);
 };
