ce826b0257e2af3bc09b8b2ea901b6644357f29f
tam11a <ibrahimsadiktamim@gmail.com>
2022-12-25 03:53:29 +0600

    added: icon on gradient

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,14 @@
+import { MdClose } from "react-icons/md";
 const Home = () => {
 	return (
 		<div>
 			<h1 className="text-3xl text-transparent text-8xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
 				Hello world!
+				<MdClose
+					style={{
+						fontSize: "2rem",
+					}}
+				/>
 			</h1>
 			<button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
 				Save Changes
